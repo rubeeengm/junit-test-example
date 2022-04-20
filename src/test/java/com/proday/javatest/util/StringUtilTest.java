fdcfd55081f343bf128f1aff481acce69243856d
Ruben Malaga <rubeeengm@outlook.com>
2022-04-19 23:32:34 -0500

    + creacion de test lanzando una excepción para alertar sobre los errores

--- src/test/java/com/proday/javatest/util/StringUtilTest.java
+++ src/test/java/com/proday/javatest/util/StringUtilTest.java
@@ -7,7 +7,14 @@ public class StringUtilTest {
 
     public static void main(String[] args) {
         String result = StringUtil.repeat("hola", 3);
+        assertEquals(result, "holaholahola");
+    }
 
-        System.out.println(result);
+    private static void assertEquals(String actual, String expected) {
+        if (!actual.equals(expected)) {
+            throw new RuntimeException(
+                actual + " is not equal to expected " + expected
+            );
+        }
     }
 }
